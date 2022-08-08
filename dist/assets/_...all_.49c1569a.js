25a70b3f9f0a636e87146bdd013db85852926dbc
Fernando Costa <fernando.bonbisu@gmail.com>
2022-08-08 03:14:49 -0300

    fix(deploy): Change vitessg to vite

---
+++ dist/assets/_...all_.49c1569a.js
@@ -0,0 +1,1 @@
+import{d as e,a as o,o as a,c as s,t as c,f as l,l as n}from"./index.53d3eab6.js";const r=e({__name:"[...all]",setup(f){const{t}=o();return(p,u)=>(a(),s("div",null,c(l(t)("not-found")),1))}});typeof n=="function"&&n(r);export{r as default};
