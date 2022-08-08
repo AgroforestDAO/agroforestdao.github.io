8761ffd4f936037e7d63d558d3ab91954aa44836
Fernando Costa <fernando.bonbisu@gmail.com>
2022-08-08 02:42:32 -0300

    feat(hero): Add BaseHero

---
+++ src/components/Atoms/BaseHero.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+interface Props {
+  title: string
+  subtitle?: string
+}
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <div class=" bg-white border-b-3 border-black dark:border-white h-[90vh] text-end px-20">
+    <div class="h-[90vh] py-[50vh]">
+      <BaseTitle>
+        {{ props.title }}
+      </BaseTitle>
+      <BaseSubtitle>
+        {{ props.subtitle }}
+      </BaseSubtitle>
+    </div>
+  </div>
+</template>
