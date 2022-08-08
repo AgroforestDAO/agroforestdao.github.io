e4e946e217c763a954fbb852148f16e14184447b
Fernando Costa <fernando.bonbisu@gmail.com>
2022-08-08 02:46:07 -0300

    feat(section): Add SocialSection

---
+++ src/components/Atoms/SocialIcon.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+interface Props {
+  label?: string
+  icon?: string
+  routeTo?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  icon: 'i-clarity:tree-solid',
+  routeTo: '/',
+})
+const go = (route = '') => {
+  window.open(route, '_blank')
+}
+</script>
+
+<template>
+  <div class="cursor-pointer" @click="go(routeTo)">
+    <slot>
+      <div class="text-3xl m-auto" :class="icon" />
+    </slot>
+    <p class="pt-2">
+      {{ props.label }}
+    </p>
+  </div>
+</template>
---
+++ src/components/Organisms/Sections/SocialSection.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="h-[50vh] flex items-center justify-center space-x-20">
+    <SocialIcon label="Discord" icon="i-fa-brands-discord" route-to="https://discord.gg/2ZchhEPA7K" />
+    <SocialIcon label="Telegram" icon="i-fa-brands-telegram" route-to="https://t.co/w4MUungCn7" />
+    <SocialIcon label="Twitter" icon="i-fa-brands-twitter" route-to="https://twitter.com/agroforestDAO" />
+  </div>
+</template>
+<!-- -->
