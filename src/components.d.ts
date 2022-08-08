c68339f3dfda56518f80269ef92a8e6907804b32
Fernando Costa <fernando.bonbisu@gmail.com>
2022-08-07 21:06:49 -0300

    clean(components): Remove template components

--- src/components.d.ts
+++ src/components.d.ts
@@ -7,6 +7,7 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
+    BaseButton: typeof import('./components/Atoms/BaseButton.vue')['default']
     Counter: typeof import('./components/Counter.vue')['default']
     Footer: typeof import('./components/Footer.vue')['default']
     README: typeof import('./components/README.md')['default']
