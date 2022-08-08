13cd676b16a0e8c3a13fe458187898f2fa30ae3b
Fernando Costa <fernando.bonbisu@gmail.com>
2022-08-08 02:42:07 -0300

    style(favicon): Remove vitesse favicon

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,24 @@
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
 useHead({
-  title: 'Vitesse',
+  title: 'AgroforestDAO',
   meta: [
-    { name: 'description', content: 'Opinionated Vite Starter Template' },
+    { name: 'description', content: '' },
     {
       name: 'theme-color',
       content: computed(() => isDark.value ? '#00aba9' : '#ffffff'),
     },
   ],
   link: [
+    // {
+    //   rel: 'icon',
+    //   type: 'image/svg+xml',
+    //   href: computed(() => preferredDark.value ? '/favicon-dark.svg' : '/favicon.svg'),
+    // },
     {
       rel: 'icon',
-      type: 'image/svg+xml',
-      href: computed(() => preferredDark.value ? '/favicon-dark.svg' : '/favicon.svg'),
+      type: 'image/png',
+      href: '/agroforestdao_logo_notitle.png',
     },
   ],
 })
