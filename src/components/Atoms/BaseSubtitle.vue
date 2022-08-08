3300cf98944fea9be69f7057d7ca657afeeb37fb
Fernando Costa <fernando.bonbisu@gmail.com>
2022-08-08 02:43:09 -0300

    feat(title): Add BaseTitle and BaseSubtitle

---
+++ src/components/Atoms/BaseSubtitle.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+interface Props {
+  label?: string
+}
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <h2 class="text-3xl">
+    <slot>
+      {{ props.label }}
+    </slot>
+  </h2>
+</template>
---
+++ src/components/Atoms/BaseTitle.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+interface Props {
+  label?: string
+
+}
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <h1 class="text-7xl font-bold">
+    <slot>
+      {{ props.label }}
+    </slot>
+  </h1>
+</template>
