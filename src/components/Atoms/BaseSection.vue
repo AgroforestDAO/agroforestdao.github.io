a4757b19980373137eafe49e784c78ad722f0daa
Fernando Costa <fernando.bonbisu@gmail.com>
2022-08-09 19:20:56 -0300

    style(section): Fix title spacing on BaseSection

--- src/components/Atoms/BaseSection.vue
+++ src/components/Atoms/BaseSection.vue
@@ -13,11 +13,16 @@ const sizes = {
   half: ['h-[50vh]'],
   quarter: ['h-[25vh]'],
 }
+const titleClasses = {
+  screen: ['text-3xl mb-7'],
+  half: ['text-2xl mb-7'],
+  quarter: ['text-xl mb-7'],
+}
 </script>
 
 <template>
-  <div class="flex flex-col items-center justify-center space-y-14" :class="[...sizes[size]]">
-    <h2 class="subtitle brut-padding">
+  <div class="flex flex-col items-center justify-center" :class="[...sizes[size]]">
+    <h2 class="subtitle brut-padding" :class="[...titleClasses[size]]">
       {{ props.title }}
     </h2>
     <slot />
