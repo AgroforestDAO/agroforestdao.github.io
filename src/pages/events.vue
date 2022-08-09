dafb68eb922c79490c41dec4a36b94f576dbc513
Fernando Costa <fernando.bonbisu@gmail.com>
2022-08-09 19:02:11 -0300

    feat(pages): Add Events page

---
+++ src/pages/events.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="pt-20 green-section">
+    <BaseHero title="Events" subtitle="Incoming Events, Meetups, News and other" background-image="https://www.ldc.com/wp-content/uploads/Uganda-coffee-agroforestry-training-scaled.jpg" />
+    <div class="px-14 green-section min-h-screen" />
+  </div>
+</template>
+
+<route lang="yaml">
+meta:
+  layout: landing
+</route>
