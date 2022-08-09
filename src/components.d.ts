1407492fbe3bcd657d7abc61b3c2034099361d00
Fernando Costa <fernando.bonbisu@gmail.com>
2022-08-09 19:07:08 -0300

    types(components): Update types

--- src/components.d.ts
+++ src/components.d.ts
@@ -7,15 +7,19 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
+    AboutSection: typeof import('./components/Organisms/Sections/AboutSection.vue')['default']
     BaseButton: typeof import('./components/Atoms/BaseButton.vue')['default']
     BaseHero: typeof import('./components/Atoms/BaseHero.vue')['default']
+    BaseSection: typeof import('./components/Atoms/BaseSection.vue')['default']
     BaseSubtitle: typeof import('./components/Atoms/BaseSubtitle.vue')['default']
     BaseTitle: typeof import('./components/Atoms/BaseTitle.vue')['default']
-    Counter: typeof import('./components/Counter.vue')['default']
-    Footer: typeof import('./components/Footer.vue')['default']
+    HighlightSection: typeof import('./components/Organisms/Sections/HighlightSection.vue')['default']
+    InitiativesSection: typeof import('./components/Organisms/Sections/InitiativesSection.vue')['default']
+    LandingFooter: typeof import('./components/Organisms/Footers/LandingFooter.vue')['default']
     LandingHeader: typeof import('./components/Organisms/Headers/LandingHeader.vue')['default']
     MenuItem: typeof import('./components/Atoms/MenuItem.vue')['default']
     README: typeof import('./components/README.md')['default']
+    RoadmapSection: typeof import('./components/Organisms/Sections/RoadmapSection.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     SocialIcon: typeof import('./components/Atoms/SocialIcon.vue')['default']
