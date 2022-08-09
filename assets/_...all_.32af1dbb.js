20c7c1eb5bdf55a5b61fbc4cb0f947e0fbce9031
Fernando Costa <fernando.bonbisu@gmail.com>
2022-08-09 19:22:13 -0300

    build(dist): Build to publish on gh-pages

---
+++ assets/_...all_.32af1dbb.js
@@ -0,0 +1,1 @@
+import{d as e,a as o,o as a,c as s,t as c,f as r,m as n}from"./index.2cd345f0.js";const f=e({__name:"[...all]",setup(l){const{t}=o();return(p,u)=>(a(),s("div",null,c(r(t)("not-found")),1))}});typeof n=="function"&&n(f);export{f as default};
