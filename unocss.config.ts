6a918e2264f36c8cd38c61e4db5434ee12a7e81e
Fernando Costa <fernando.bonbisu@gmail.com>
2022-08-09 19:04:52 -0300

    style(config): Update unocss theme config

--- unocss.config.ts
+++ unocss.config.ts
@@ -16,9 +16,10 @@ export default defineConfig({
     ['title', 'text-3xl md:text-5xl font-bold tracking-tight leading-tight'],
     ['subtitle', 'text-lg md:text-3xl font-semibold tracking-tight leading-tight'],
     ['green-section', 'bg-light dark:bg-dark text-stone-700 dark:text-stone-300'],
-    ['gray-section', 'bg-stone-300 dark:bg-stone-700 text-dark dark:text-light'],
+    ['brown-section', 'bg-stone-300 dark:bg-stone-700 text-dark dark:text-light'],
     ['section-border', 'border-b-3 border-stone-700 dark:border-stone-300'],
     ['brut-padding', 'bg-white dark:bg-stone-700 py-2 px-3'],
+    ['brut-padding-text', 'text-stone-700 dark:text-white'],
   ],
   presets: [
     presetUno(),
